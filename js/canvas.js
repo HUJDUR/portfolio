6abb87c9822aaa57092d974f8781ec9577d2dd75
HUJDUR <nadir.hujdur2002@gmail.com>
2022-01-11 21:31:07 +0100

    Update

--- js/canvas.js
+++ js/canvas.js
@@ -1,69 +1,23 @@
 const canvas = document.getElementById('canvas');
 const header = document.getElementsByClassName('header');
-canvas.height = innerHeight + 100;
+canvas.height = innerHeight;
 canvas.width = innerWidth;
 const c = canvas.getContext('2d');
 
-const Circle = function (x, y, radius, color) {
-	this.x = x;
-	this.y = y;
-	this.radius = radius;
-	this.r = 0;
-	this.color = color;
-
-	this.draw = function () {
-		c.beginPath();
-		c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-		c.fillStyle = this.color;
-		c.fill();
-		c.stroke();
-	};
-
-	this.update = function () {
-		// c.clearRect(0, 0, innerHeight, innerWidth);
-		if (this.radius > this.r) this.r += 0.5;
-		this.draw();
-	};
-};
-
-const StraightLine = function (x, y, end, dy, color) {
-	this.x = x;
-	this.y = y + 100;
-	this.end = end;
-	this.now = innerHeight;
-	this.dy = dy;
-	this.color = color;
-
-	this.draw = function () {
-		c.beginPath();
-		c.moveTo(this.x, this.y);
-		c.lineTo(this.x, this.now);
-		c.strokeStyle = this.color;
-		c.stroke();
-	};
-
-	this.update = function () {
-		c.clearRect(0, 0, innerHeight, innerWidth);
-		if (this.now > this.end) this.now -= this.dy;
-		this.draw();
-	};
-};
-
-const line = new StraightLine(
-	innerWidth / 4,
-	innerHeight,
-	innerHeight / 3,
-	9,
-	'#e9c46a'
-);
-const circle = new Circle(innerWidth / 4, innerHeight / 3, 10, '#e9c46a');
-
+let frequncy = 0.01;
+let outsideCounter = 0;
 const animate = function () {
-	line.update();
-	circle.update();
 	requestAnimationFrame(animate);
+
+	c.clearRect(0, 0, canvas.width, canvas.height);
+	c.beginPath();
+	c.moveTo(canvas.width / 5, 0);
+
+	for (let i = 0; i < canvas.height; i++) {
+		c.lineTo(canvas.width / 5 + Math.sin(i * -0.003 + frequncy) * 35, i);
+	}
+	c.stroke();
+	frequncy += 0.01;
 };
 
-// setTimeout(() => {
 animate();
-// }, 2500);
